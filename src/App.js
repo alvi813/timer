b0e777f0fa57604dc43ca5e91a155a005bb2b089
Victoriya <alvi813@yandex.ru>
2020-07-20 01:52:59 +0300

    add class App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from "react";
+import Timer from "./components/timer";
 
-import './App.css';
+import './App.css'
 
 
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showTimer: true
+    }
+  }
+
+  toggleTimer = () => {
+    this.setState({showTimer: !this.state.showTimer});
+  }
+
+  render() {
+    const {showTimer} = this.state;
+    return (
+        <div className="mainDiv">
+          <button className="btn btn-outline-success hide-show-timer-btn" onClick={this.toggleTimer}>
+            {showTimer ? 'Hide' : 'Show'} timer
+          </button>
+          {showTimer && <Timer/>}
+        </div>
+    )
+  }
 }
 
 export default App;
